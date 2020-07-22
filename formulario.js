08a7bfe2c94ac271ec68ec359727289db4d0ac7d
Wladimir Guevara <wladige@gmail.com>
2020-07-21 23:27:23 -0400

    terminacion de los inputs y estilos

---
+++ formulario.js
@@ -0,0 +1,7 @@
+// Funcion Validar 
+
+function Validar(){
+    let rut = document.getElementById('Rut')
+    let nombre = document.getElementById('Nombres')
+    let apellido = document.getElementById('Apellidos')
+}
