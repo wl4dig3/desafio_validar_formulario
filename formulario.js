f69ecc51b5fbf0d44f09e74677e8453551865e28
Wladimir Guevara <wladige@gmail.com>
2020-07-26 19:01:05 -0400

    errores corregidos

--- formulario.js
+++ formulario.js
@@ -2,87 +2,93 @@
 
 function validar() {
     
-    let rut = document.getElementById('Rut').value;
-    let nombre = document.getElementById('Nombre').value;
-    let apellido = document.getElementById('Apellido').value;
-    let edad = document.getElementById('Edad').value;
-    let correo = document.getElementById('Correo').value;
-    let select = document.getElementById('Select').value;
-    let Fecha=  new Date();
-    Fecha.getDate() + "-"+ Fecha.getMonth()+ "-" +Fecha.getFullYear();
-    let hora = document.getElementById('Hora').value;
+    let rut = document.getElementById('rut').value;
+    let nombre = document.getElementById('nombre').value;
+    let apellido = document.getElementById('apellido').value;
+    let edad = document.getElementById('edad').value;
+    let correo = document.getElementById('correo').value;
+    let select = document.getElementById('select').value;
+    let fecha = document.getElementById('fecha').value;
+    let hora = document.getElementById('hora').value;
 
     // expresion regular rut 
-    const expRut = /^\d{1,2}\.\d{3}\.\d{3}[-][0-9kK]{1}$/;
+         expRut = /^\d{1,2}\.\d{3}\.\d{3}[-][0-9kK]{1}$/;
     // expresion regular nombre 
-    const  expNombre = /^[a-zA-Z\ áéíóúÁÉÍÓÚñÑ\s]*$/;
+          expNombre = /^[a-zA-Z\ áéíóúÁÉÍÓÚñÑ\s]*$/;
     // expresion regular apellido
-    const expApellido = /^[a-zA-Z\ áéíóúÁÉÍÓÚñÑ\s]*$/;
+        expApellido = /^[a-zA-Z\ áéíóúÁÉÍÓÚñÑ\s]*$/;
     // expresion regular edad
-    const expEdad = /^\d*$/;
+         expEdad = /\d{2}/;
     // expresion regular correo
-    const expCorreo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+         expCorreo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
     // expresion regular fecha
-    const expFecha = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+         expFecha = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
 
 
-    if(Rut.value === "" || 
-    Nombre.value === "" ||
-    Apellido.value === "" || 
-    Edad.value === "" ||
-    Correo.value === "" ||
-    Select.value === "" ||
-    Fecha.value === "" ||
-    Hora.value === "" ) {
+    if(rut.value === "" || 
+    nombre.value === "" ||
+    apellido.value === "" || 
+    edad.value === "" ||
+    correo.value === "" ||
+    select.value === "" ||
+    fecha.value === "" ||
+    hora.value === "" ) {
         alert('Favor llenar todos los campos');
         return false;
     }
     // Condicionales 
-    else if (expRut.test(Rut)){
+    if (!expRut.test(rut)){
         alert('Ingrese rut válido');
         return false;
     }
-    else if (Nombre.value.length >10) {
+    else if (nombre.length >10) {
         alert('Nombre excede de 10 caracteres');
         return false;
     }
-    else if (expNombre.test(Nombre)){
+    else if (!expNombre.test(nombre)){
         alert('Ingrese un nombre válido');
         return false;
     }
-    else if (Apellido.value.length >10){
+    else if (apellido.length >10){
         alert('Apellido excede de 10 caracteres');
         return false;
     }
-    else if(expApellido.test(Apellido)){
+    else if(!expApellido.test(apellido)){
         alert('Ingrese un apellido válido');
         return false;
     }
-    else if(Edad.value.length >3){
+    else if(edad.length >3){
         alert('Imposible que tengas más de 100 años,\n bueno, casi imposible');
         return false;
     }
-    else if (isNaN(Edad.value)){
-        alert('Debe ser sólo números');
+    // else if (isNaN(edad.value)){
+    //     alert('Debe ser sólo números');
+    //     return false;
+    // }
+    else if (!expEdad.test(edad)){
+        alert('Edad no valida');
         return false;
     }
-    else if (expEdad.test(Edad)){
-        alert('Ingrese sólo números');
-        return false;
-    }
-    else if(Correo.value.length >20) {
+    else if(correo.length >20) {
         alert('Correo electronico muy largo');
         return false;
     }
-    else if (expCorreo.test(Correo)){
+    else if (!expCorreo.test(correo)){
         alert('Ingrese un formato de correo válido')
         return false
     }
+    else if (!expFecha.test(fecha)){
+        alert('Ingrese una fecha válida')
+        return false
+    }
     else {
-        alert('Gracias por completar todo los datos Sr(a) ' + Nombre );
+        alert('Gracias por completar todo los datos Sr(a) ');
+        return false
     }
 
 
+    // funcion para imprimir el texto 
+    function imprimir(){
+        
+    }
 }
-    
-// (!expresionApellido.test(apellido));
